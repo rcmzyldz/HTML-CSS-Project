a747e56d5334101b39bdab051fc75882cd4b64e2
Rahime Yildiz <rahime1yildiz@gmail.com>
2022-02-11 09:27:26 +0100

    add javascript file

---
+++ app.js
@@ -0,0 +1,12 @@
+let tab = document.getElementById("table-container");
+
+let btn =document.getElementById("btn"); 
+
+function getTable(){
+   
+   tab.style.visibility = "visible";
+   btn.style.visibility = "hidden";
+    return tab;
+}
+
+
